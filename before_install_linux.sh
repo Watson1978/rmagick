3ee36f2cd481d74b39b4a327cf39272cb9d1fae9
Watson <watson1978@gmail.com>
2019-04-01 22:41:58 +0900

    CI: Use built cache to skip compiling on Travis

--- before_install_linux.sh
+++ before_install_linux.sh
@@ -1,4 +1,4 @@
-set -euo pipefail
+set -euox pipefail
 
 sudo apt-get update
 
@@ -15,35 +15,50 @@ if [ ! -d /usr/include/freetype ]; then
   sudo ln -sf /usr/include/freetype2 /usr/include/freetype
 fi
 
+dir_options=""
+if [ -v CONFIGURE_OPTIONS ]; then
+  dir_options=$CONFIGURE_OPTIONS
+fi
+
 if [ -v IMAGEMAGICK_VERSION ]; then
-  version=(${IMAGEMAGICK_VERSION//./ })
-  if (( "${version[0]}${version[1]}" >= 69 )); then
-    wget https://github.com/ImageMagick/ImageMagick6/archive/${IMAGEMAGICK_VERSION}.tar.gz
-    tar -xzf ${IMAGEMAGICK_VERSION}.tar.gz
-    cd ImageMagick6-${IMAGEMAGICK_VERSION}
+  if [ -d build-ImageMagick/ImageMagick-${IMAGEMAGICK_VERSION}-${dir_options} ]; then
+    cd build-ImageMagick/ImageMagick-${IMAGEMAGICK_VERSION}-${dir_options}
   else
-    wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
-    tar -xf ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
-    cd ImageMagick-${IMAGEMAGICK_VERSION}
+    mkdir -p build-ImageMagick
+
+    version=(${IMAGEMAGICK_VERSION//./ })
+    if (( "${version[0]}${version[1]}" >= 69 )); then
+      wget https://github.com/ImageMagick/ImageMagick6/archive/${IMAGEMAGICK_VERSION}.tar.gz
+      tar -xf ${IMAGEMAGICK_VERSION}.tar.gz
+      mv ImageMagick6-${IMAGEMAGICK_VERSION} build-ImageMagick/ImageMagick-${IMAGEMAGICK_VERSION}-${dir_options}
+    else
+      wget http://www.imagemagick.org/download/releases/ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
+      tar -xf ImageMagick-${IMAGEMAGICK_VERSION}.tar.xz
+      mv ImageMagick-${IMAGEMAGICK_VERSION} build-ImageMagick/ImageMagick-${IMAGEMAGICK_VERSION}-${dir_options}
+    fi
+
+    cd build-ImageMagick/ImageMagick-${IMAGEMAGICK_VERSION}-${dir_options}
+
+    options="--with-magick-plus-plus=no --disable-docs"
+    if [ -v CONFIGURE_OPTIONS ]; then
+      options="${CONFIGURE_OPTIONS} $options"
+    fi
+
+    CC="ccache cc" CXX="ccache c++" ./configure --prefix=/usr $options
+
+    make -j
   fi
+
+  sudo make install -j
+  cd ../..
 else
   echo "you must specify an ImageMagick version."
   echo "example: 'IMAGEMAGICK_VERSION=6.8.9-10 bash ./before_install_linux.sh'"
   exit 1
 fi
 
-options="--with-magick-plus-plus=no --disable-docs"
-if [ -v CONFIGURE_OPTIONS ]; then
-  options="${CONFIGURE_OPTIONS} $options"
-fi
-
-CC="ccache cc" CXX="ccache c++" ./configure --prefix=/usr $options
-
-make -j
-sudo make install -j
-cd ..
 sudo ldconfig
 
 gem install bundler
 
-set +u
+set +ux
