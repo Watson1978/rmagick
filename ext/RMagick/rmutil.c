8cc4dc743a14f4da9d9ff7138462657420b20061
Watson <watson1978@gmail.com>
2020-01-26 20:33:54 +0900

    Add macro to get Image structure

--- ext/RMagick/rmutil.c
+++ ext/RMagick/rmutil.c
@@ -264,7 +264,7 @@ rm_check_destroyed(VALUE obj)
 {
     Image *image;
 
-    Data_Get_Struct(obj, Image, image);
+    GetImageStruct(obj, image);
     if (!image)
     {
         rb_raise(Class_DestroyedImageError, "destroyed image");
