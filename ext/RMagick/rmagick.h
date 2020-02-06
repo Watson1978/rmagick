0ce3ac82901343af7530d183bbc81fe1d5956894
Watson <watson1978@gmail.com>
2020-02-07 01:18:50 +0900

    tt

--- ext/RMagick/rmagick.h
+++ ext/RMagick/rmagick.h
@@ -39,11 +39,11 @@
     #if SIZEOF_SIZE_T == SIZEOF_LONG
     # define RMIuSIZE "lu"
     # define RMIdSIZE "ld"
-    # define PMIsVALUE "%li\v"
+    # define PMIsVALUE "%li"
     #elif SIZEOF_SIZE_T == SIZEOF_LONG_LONG
     # define RMIuSIZE "I64u"
     # define RMIdSIZE "I64d"
-    # define PMIsVALUE "%I64i\v"
+    # define PMIsVALUE "%I64i"
     #endif
 #else
     # define RMIuSIZE PRIuSIZE
