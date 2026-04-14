46cbcfa496ec0d01023c54d2c251d9a00f0a1b25
Le1gh <Leigharber@gmail.com>
2026-04-13 22:48:47 -0500

    Fix floating-point truncation in Magick::ChromaticityInfo coordinates (#1241)
    * Flesh out specs for Image#chromaticity, chromaticity=
    
    We discovered weird behavior with chromaticity where it loses the floating point values. This seemed wrong so we changed the function `Import_PrimaryInfo` in
    `rmstruct.c` from an integer conversion to a floating point conversion. This function is only used when instantiating a new chromaticity.
    
    We have questions:
    
    * Should this be floating point?
    * Why are the numbers different in the test (`0.64` comes out as `0.63999998...`)
    * Why are the z values of the `Primary` objects being set to 0 in `Export_ChromaticityInfo`?
    
    * Add missing frozen_string_literal magic comment
    
    * Fix spec
    
    ---------
    
    Co-authored-by: Shizuo Fujita <fujita@clear-code.com>

--- spec/rmagick/image/chromaticity_spec.rb
+++ spec/rmagick/image/chromaticity_spec.rb
@@ -1,25 +1,48 @@
 # frozen_string_literal: true
 
 RSpec.describe Magick::Image, '#chromaticity' do
-  it 'works' do
-    image = described_class.new(100, 100)
+  it "returns an assigned chromaticity" do
+    image = build_image
 
-    chrom = image.chromaticity
-    expect { image.chromaticity }.not_to raise_error
-    expect(chrom).to be_instance_of(Magick::Chromaticity)
-    expect(chrom.red_primary.x).to eq(0)
-    expect(chrom.red_primary.y).to eq(0)
-    expect(chrom.red_primary.z).to eq(0)
-    expect(chrom.green_primary.x).to eq(0)
-    expect(chrom.green_primary.y).to eq(0)
-    expect(chrom.green_primary.z).to eq(0)
-    expect(chrom.blue_primary.x).to eq(0)
-    expect(chrom.blue_primary.y).to eq(0)
-    expect(chrom.blue_primary.z).to eq(0)
-    expect(chrom.white_point.x).to eq(0)
-    expect(chrom.white_point.y).to eq(0)
-    expect(chrom.white_point.z).to eq(0)
-    expect { image.chromaticity = chrom }.not_to raise_error
-    expect { image.chromaticity = 2 }.to raise_error(TypeError)
+    red_primary = Magick::Primary.new(70, 200, 0)
+    green_primary = Magick::Primary.new(200, 44, 0)
+    blue_primary = Magick::Primary.new(60, 22, 0)
+    white_point = Magick::Primary.new(77, 43, 0)
+    chromaticity = Magick::Chromaticity.new(red_primary, green_primary, blue_primary, white_point)
+    image.chromaticity = chromaticity
+
+    expect(image.chromaticity.red_primary).to eq(red_primary)
+    expect(image.chromaticity.green_primary).to eq(green_primary)
+    expect(image.chromaticity.blue_primary).to eq(blue_primary)
+    expect(image.chromaticity.white_point).to eq(white_point)
+  end
+
+  it "returns default chromaticity values" do
+    image = build_image
+    delta = 0.0001
+
+    expect(image.chromaticity.red_primary).to have_attributes(
+      x: be_within(delta).of(0.64),
+      y: be_within(delta).of(0.33),
+      z: be_within(delta).of(0.03)
+    )
+
+    expect(image.chromaticity.green_primary).to have_attributes(
+      x: be_within(delta).of(0.30),
+      y: be_within(delta).of(0.60),
+      z: be_within(delta).of(0.10)
+    )
+
+    expect(image.chromaticity.blue_primary).to have_attributes(
+      x: be_within(delta).of(0.15),
+      y: be_within(delta).of(0.06),
+      z: be_within(delta).of(0.79)
+    )
+
+    expect(image.chromaticity.white_point).to have_attributes(
+      x: be_within(delta).of(0.3127),
+      y: be_within(delta).of(0.3290),
+      z: be_within(delta).of(0.3583)
+    )
   end
 end
